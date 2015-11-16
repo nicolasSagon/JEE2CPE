80b0909899dc84ae7b1053c9b1738ed2e7454ecd
nicolasSagon <nicolas.sagon@tpc16.cpe.fr>
2015-11-16 17:21:10 +0100

    TP1

---
+++ EmployeeApp/src/com/gui/controllers/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.gui.controllers;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+
+import com.ejb.services.EmployeeService;
+import com.jpa.entities.Employee;
+
+@ManagedBean
+public class EmployeeController {
+
+	private Employee employee = new Employee();
+
+	@EJB
+	private EmployeeService service;
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	public void saveEmployee(Employee emp) {
+		service.addEmployee(emp);
+	}
+
+}
